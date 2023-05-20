0aa69397ed850d726a7464e35b777ce6e0614450
YuxuanLiu <franklyx313@gmail.com>
2023-05-19 21:54:31 -0400

    set up Django admin

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,42 @@
+"""
+
+Tests for Django Admin modifications.
+"""
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """Test admin site"""
+
+    def setUp(self):
+        """Ctreate a test client and a superuser"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@example.com",
+            password="testpass123",
+        )
+
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="user@example.com",
+            password="testpass123",
+            name="Test User",
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse("admin:core_user_changelist")
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test that the edit user page works"""
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
